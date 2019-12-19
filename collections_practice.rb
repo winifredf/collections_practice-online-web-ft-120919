a4f842a5ddb31a7f2ab10b47ec33be6eead75c38
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 08:10:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,11 @@ def sort_array_asc(array)
   array.sort
 end
 
+def sort_array_asc(array)
+  array.sosrt {|x,y| y <=> x}
+end
+
+def sort_array_asc(array)
+  array [1], array[2], = array[2], array[1]
+  array
+end
