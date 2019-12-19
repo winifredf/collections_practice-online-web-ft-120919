500746b244037832e659d3a5dae1ebed8cb94615
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 08:48:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,12 @@ def sort_array_desc(array)
   array.sort {|num1,num2| num2 <=> num1}
 end
 
+
+def sort_array_char_count(array)
+  array.sort {|left, right| left.length <=> right.length}
+end
+
+
 def swap_elements(array)
   array[1], array[2], = array[2], array[1]
   array
