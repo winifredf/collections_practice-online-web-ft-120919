c037e047803182eaadb81720fd9f4c70af4dd87c
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 08:52:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,24 +24,9 @@ def kesha_maker(array)
   end
 end
 
-def greater_and_less_than_10(array)
-  hash = {"greater_and_less_than_10" => [], "less_than_10" => []}
-  array.each do |number|
-    if number > 10
-      hash["greater_and_less_than_10"] << number
-    else
-      hash["less_than_10"] << number
-    end
-  end
-  hash
-end
-
-def find_winners(hash)
-  winners = []
-  hash.each do |name, status|
-    winners << name if status == "winner"
-  end
-  winners
+def reverse_array(array)
+  new_array = array.reverse_array
+  new_array
 end
 
 def find_a(array)
@@ -54,31 +39,3 @@ def find_a(array)
   end
 end
 
-def count_words(words)
-  story_count = Hash.new
-  
-  words.split.each do |word|
-    if story_count.include?(word)
-      story_count[word] = story_count[word]
-    else
-      story_count[word] = 1
-    end
-  end
-  
-  story_count
-end
-
-def organize_songs_by_artist(track)
-  hash = {}
-  tracks.each do |name|
-    artist = name.split(" - ")[0]
-    trackname = name.split(" - ")[1]
-    
-    if hash.include?(artist)
-      hash[artist] << trackname
-    else
-      hash[artist] = [] << trackname
-    end
-  end
-  hash
-end
