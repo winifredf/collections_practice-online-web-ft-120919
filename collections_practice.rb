6188d396e2905db9a6e87480ab3b3e95cbdb17c4
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 09:06:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|num1,num2| num2 <=> num1}
+end
+
+
+def sort_array_char_count(array)
+  array.sort {|left, right| left.length <=> right.length}
+end
+
+
+def swap_elements(array)
+  array[1], array[2], = array[2], array[1]
+  array
+end
+
+def kesha_maker(array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def reverse_array(array)
+  new_array = array.reverse
+  new_array
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(numbers)
+  numbers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
